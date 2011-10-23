4e64ec519dd13b1ea0a1dd5feb4a66d5cf3190f7
Jason Felice <jason.m.felice@gmail.com>
2011-10-23 12:17:29 -0400

    Implement computeCost

--- mlclass/mlclass-ex1/computeCost.m
+++ mlclass/mlclass-ex1/computeCost.m
@@ -3,20 +3,15 @@ function J = computeCost(X, y, theta)
 %   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
 %   parameter for linear regression to fit the data points in X and y
 
-% Initialize some useful values
-m = length(y); % number of training examples
-
-% You need to return the following variables correctly 
-J = 0;
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+h = theta(1) + theta(2) * X(:,2);
+J = sum((h .- y) .^ 2) / (2 * length(y));
 
 % =========================================================================
 
 end
+
+% vim:set ft=octave sts=4 sw=4 ai et:
