06b88a5e5888b5c29abd191fb5448249b14fe505
Jason Felice <jason.m.felice@gmail.com>
2011-10-23 12:18:44 -0400

    Implement gradient descent

--- mlclass/mlclass-ex1/gradientDescent.m
+++ mlclass/mlclass-ex1/gradientDescent.m
@@ -16,11 +16,13 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+    
+    h = theta(1) + theta(2) * X(:,2);
+    a = alpha / m * sum( (h .- y) .* X(:,1) );
+    b = alpha / m * sum( (h .- y) .* X(:,2) );
 
-
-
-
-
+    theta(1) -= a;
+    theta(2) -= b;
 
 
     % ============================================================
@@ -31,3 +33,4 @@ for iter = 1:num_iters
 end
 
 end
+% vim:set ft=octave sts=4 sw=4 ai et:
