df7de99e8ac014b7c7d4ffd95ca437fabc451afc
Jason Felice <jason.m.felice@gmail.com>
2011-10-30 22:48:25 -0400

    predict() implemented

--- mlclass/mlclass-ex2/predict.m
+++ mlclass/mlclass-ex2/predict.m
@@ -15,13 +15,10 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
-
-
-
-
-
+p = sigmoid(X * theta) >= 0.5;
 
 % =========================================================================
 
 
 end
+% vim:set ft=octave sts=4 sw=4 ai et:
