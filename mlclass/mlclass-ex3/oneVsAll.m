49c5c7dc01604c2a7e883149c5e678d5d7b7df27
Jason Felice <jason.m.felice@gmail.com>
2011-11-05 18:51:21 -0400

    Implement oneVsAll

--- mlclass/mlclass-ex3/oneVsAll.m
+++ mlclass/mlclass-ex3/oneVsAll.m
@@ -7,15 +7,6 @@ function [all_theta] = oneVsAll(X, y, num_labels, lambda)
 %   in a matrix all_theta, where the i-th row of all_theta corresponds 
 %   to the classifier for label i
 
-% Some useful variables
-m = size(X, 1);
-n = size(X, 2);
-
-% You need to return the following variables correctly 
-all_theta = zeros(num_labels, n + 1);
-
-% Add ones to the X data matrix
-X = [ones(m, 1) X];
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: You should complete the following code to train num_labels
@@ -49,18 +40,24 @@ X = [ones(m, 1) X];
 %                 initial_theta, options);
 %
 
+% Some useful variables
+m = size(X, 1);
+n = size(X, 2);
 
+% You need to return the following variables correctly 
+all_theta = zeros(num_labels, n + 1);
 
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
 
+  initial_theta = zeros(n + 1, 1);
+  options = optimset('GradObj', 'on', 'MaxIter', 50);
 
-
-
-
-
-
-
+  for c=1:num_labels
+    [theta] = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
+    all_theta(c,:) = theta';
+  end
 
 % =========================================================================
-
 
 end
