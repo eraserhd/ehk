45157ab0de2ad60f98a1f9559bdecf9bf0134dcd
Jason Felice <jason.m.felice@gmail.com>
2011-11-13 19:25:33 -0500

    Implement randInitializeWeights

--- mlclass/mlclass-ex4/randInitializeWeights.m
+++ mlclass/mlclass-ex4/randInitializeWeights.m
@@ -10,7 +10,6 @@ function W = randInitializeWeights(L_in, L_out)
 %
 
 % You need to return the following variables correctly 
-W = zeros(L_out, 1 + L_in);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Initialize W randomly so that we break the symmetry while
@@ -19,13 +18,8 @@ W = zeros(L_out, 1 + L_in);
 % Note: The first row of W corresponds to the parameters for the bias units
 %
 
-
-
-
-
-
-
-
+epsilon_init = 0.12;
+W = rand(L_out, 1 + L_in) * 2 * epsilon_init - epsilon_init;
 
 % =========================================================================
 
