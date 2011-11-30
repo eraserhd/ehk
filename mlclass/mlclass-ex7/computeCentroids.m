cd2bf2ca60cf69b9a415d6a750f8a3395b39f846
Jason Felice <jason.m.felice@gmail.com>
2011-11-29 22:11:31 -0500

    Implement computeCentroids()

--- mlclass/mlclass-ex7/computeCentroids.m
+++ mlclass/mlclass-ex7/computeCentroids.m
@@ -25,16 +25,12 @@ centroids = zeros(K, n);
 %
 % Note: You can use a for-loop over the centroids to compute this.
 %
-
-
-
-
-
-
-
+for i=1:K
+  select = (idx == i);
+  centroids(i,:) = sum(select * ones(1, n) .* X, 1) ./ sum(select);
+end
 
 % =============================================================
-
 
 end
 
