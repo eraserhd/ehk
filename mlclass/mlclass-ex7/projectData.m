08581afc6e013ddd0287ee0215f2972e4685d753
Jason Felice <jason.m.felice@gmail.com>
2011-11-30 23:17:50 -0500

    Some cleanup, implement recoverData

--- mlclass/mlclass-ex7/projectData.m
+++ mlclass/mlclass-ex7/projectData.m
@@ -6,9 +6,6 @@ function Z = projectData(X, U, K)
 %   the first K columns of U. It returns the projected examples in Z.
 %
 
-% You need to return the following variables correctly.
-Z = zeros(size(X, 1), K);
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the projection of the data using only the top K 
 %               eigenvectors in U (first K columns). 
@@ -18,8 +15,7 @@ Z = zeros(size(X, 1), K);
 %                    projection_k = x' * U(:, k);
 %
 
-
-
+Z = X * U(:, 1:K);
 
 % =============================================================
 
--- mlclass/mlclass-ex7/recoverData.m
+++ mlclass/mlclass-ex7/recoverData.m
@@ -7,7 +7,7 @@ function X_rec = recoverData(Z, U, K)
 %
 
 % You need to return the following variables correctly.
-X_rec = zeros(size(Z, 1), size(U, 1));
+%X_rec = zeros(size(Z, 1), size(U, 1));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the approximation of the data by projecting back
@@ -21,7 +21,7 @@ X_rec = zeros(size(Z, 1), size(U, 1));
 %               Notice that U(j, 1:K) is a row vector.
 %               
 
-
+X_rec = Z * U(:, 1:K)';
 
 % =============================================================
 
