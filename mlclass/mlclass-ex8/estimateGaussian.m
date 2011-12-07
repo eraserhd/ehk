94edd7c296e5c5aaba69d8e07c304b25cc87c607
Jason Felice <jason.m.felice@gmail.com>
2011-12-06 22:33:50 -0500

    Implement estimateGaussian

--- mlclass/mlclass-ex8/estimateGaussian.m
+++ mlclass/mlclass-ex8/estimateGaussian.m
@@ -10,27 +10,7 @@ function [mu sigma2] = estimateGaussian(X)
 % Useful variables
 [m, n] = size(X);
 
-% You should return these values correctly
-mu = zeros(n, 1);
-sigma2 = zeros(n, 1);
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the mean of the data and the variances
-%               In particular, mu(i) should contain the mean of
-%               the data for the i-th feature and sigma2(i)
-%               should contain variance of the i-th feature.
-%
-
-
-
-
-
-
-
-
-
-
-% =============================================================
-
+mu = sum(X) / m;
+sigma2 = sum((X - (mu' * ones(1, m))') .^ 2) / m;
 
 end
