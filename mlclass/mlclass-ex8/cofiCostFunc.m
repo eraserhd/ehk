21e325ca111d852a02e14b44773ceaf25fa853ac
Jason Felice <jason.m.felice@gmail.com>
2011-12-10 22:08:59 -0500

    Implement cofi cost function

--- mlclass/mlclass-ex8/cofiCostFunc.m
+++ mlclass/mlclass-ex8/cofiCostFunc.m
@@ -10,14 +10,7 @@ function [J, grad] = cofiCostFunc(params, Y, R, num_users, num_movies, ...
 X = reshape(params(1:num_movies*num_features), num_movies, num_features);
 Theta = reshape(params(num_movies*num_features+1:end), ...
                 num_users, num_features);
-
             
-% You need to return the following values correctly
-J = 0;
-X_grad = zeros(size(X));
-Theta_grad = zeros(size(Theta));
-
-% ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost function and gradient for collaborative
 %               filtering. Concretely, you should first implement the cost
 %               function (without regularization) and make sure it is
@@ -40,22 +33,11 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+% You need to return the following values correctly
+X_grad = zeros(size(X));
+Theta_grad = zeros(size(Theta));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-% =============================================================
+J = sum(sum(((X * Theta' - Y) .* R) .^ 2, 1), 2) / 2;
 
 grad = [X_grad(:); Theta_grad(:)];
 
