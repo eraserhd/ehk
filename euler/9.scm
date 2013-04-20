a274390527b4a607458ea7eab2ff0ce7f72cc6b3
Jason Felice <jason.m.felice@gmail.com>
2013-04-19 21:24:25 -0400

    Make FOR-EACH-COMBINATION

--- euler/9.scm
+++ euler/9.scm
@@ -2,14 +2,12 @@
 (define (special-pythogorean-triplet)
   (call/cc
     (lambda (return)
-      (for-each
-	(lambda (a)
-	  (for-each
-	    (lambda (b)
-	      (let ((c (- 1000 a b)))
-		(if (= (+ (* a a) (* b b)) (* c c))
-		  (return (list a b c)))))
-	    (range from: 1 to: 1000)))
+      (for-each-combination
+	(lambda (a b)
+	  (let ((c (- 1000 a b)))
+	    (if (= (+ (* a a) (* b b)) (* c c))
+	      (return (list a b c)))))
+	(range from: 1 to: 1000)
 	(range from: 1 to: 1000)))))
 
 (pp (apply * (special-pythogorean-triplet)))
