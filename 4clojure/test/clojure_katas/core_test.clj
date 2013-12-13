082fb797c8d9a187c74759102478e9079ad445fe
Jason Felice <jason.m.felice@gmail.com>
2013-12-12 22:40:59 -0500

    Initial commit

---
+++ 4clojure/test/clojure_katas/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-katas.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-katas.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
