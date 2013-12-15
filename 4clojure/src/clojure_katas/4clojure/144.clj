75778862ad1a382c94fc7e3191500d35548ecd3e
Jason Felice <jason.m.felice@gmail.com>
2013-12-14 20:45:51 -0500

    4clojure 144 (lazy-seq, yay!)

---
+++ 4clojure/src/clojure_katas/4clojure/144.clj
@@ -0,0 +1,12 @@
+(ns clojure-katas.4clojure.144)
+
+(def oscilrate
+
+  (fn x [value f & fs]
+    (lazy-seq
+      (cons value (apply x (f value) (conj (vec fs) f)))))
+
+  )
+
+(= (take 3 (oscilrate 3.14 int double)))
+(= (take 5 (oscilrate 3 #(- % 3) #(+ 5 %))) [3 0 5 2 7])
