52fbabdfb8997990dea7154df93940def532cb63
Jason Felice <jason.m.felice@gmail.com>
2013-12-22 23:47:29 -0500

    4clojure 100

--- 4clojure/src/clojure_katas/algo.clj
+++ 4clojure/src/clojure_katas/algo.clj
@@ -15,7 +15,15 @@
             (recur (into remaining-queue unseen)
                    (into backtrack (map #(vec [% node]) unseen)))))))))
 
-(defn gcd [a b]
-  (if (= b 0)
-    a
-    (recur b (mod a b))))
+(defn gcd
+  ([]
+   1)
+  ([a]
+   a)
+  ([a b]
+   (if (= b 0)
+     a
+     (recur b (mod a b))))
+  ([a b c & more]
+   (reduce gcd (concat [a b c] more))))
+
