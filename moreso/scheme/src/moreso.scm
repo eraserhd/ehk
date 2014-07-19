44d6865d1b6d6f6bdd5324170caf60c9b42ad73a
Jason Felice <jason.m.felice@gmail.com>
2014-07-18 21:28:58 -0400

    eval sym

--- moreso/scheme/src/moreso.scm
+++ moreso/scheme/src/moreso.scm
@@ -1,14 +1,26 @@
 
+(define (moreso:lookup sym env)
+  (if (null? env)
+    #f
+    (let ((assq-result (assq sym (car env))))
+      (if assq-result
+	assq-result
+	(moreso:lookup sym (cdr env))))))
 
-(define (moreso:eval expr)
+(define (moreso:eval expr env)
   (cond
+    ((symbol? expr)
+     (let ((cell (moreso:lookup expr env)))
+       (if cell
+	 (cdr cell))))
+
     ((list? expr)
      (case (car expr)
        ;; Special forms
        ((if)
-	(if (moreso:eval (cadr expr))
-	  (moreso:eval (caddr expr))
-	  (moreso:eval (cadddr expr))))))
+	(if (moreso:eval (cadr expr) env)
+	  (moreso:eval (caddr expr) env)
+	  (moreso:eval (cadddr expr) env)))))
 
     (else
      expr)))
--- moreso/scheme/test/eval-test.scm
+++ moreso/scheme/test/eval-test.scm
@@ -1,8 +1,11 @@
 (include "expect.scm")
 (include "../src/moreso.scm")
 
-(expect (equal? 42 (moreso:eval 42)))
-(expect (equal? #f (moreso:eval #f)))
-(expect (equal? #t (moreso:eval #t)))
-(expect (equal? 79 (moreso:eval '(if #t 79))))
-(expect (equal? 86 (moreso:eval '(if #f 79 86))))
+(define e '(((forty-two . 42))))
+
+(expect (equal? 42 (moreso:eval 42 e)))
+(expect (equal? #f (moreso:eval #f e)))
+(expect (equal? #t (moreso:eval #t e)))
+(expect (equal? 79 (moreso:eval '(if #t 79) e)))
+(expect (equal? 86 (moreso:eval '(if #f 79 86) e)))
+(expect (equal? 42 (moreso:eval 'forty-two e)))
