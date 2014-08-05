87c6b377137fbe2f2885f3cff324a5894233fa44
Jason Felice <jason.m.felice@gmail.com>
2014-08-05 13:41:06 -0400

    Make a test allocator

--- moreso/scheme/tinyscheme/test_transients.c
+++ moreso/scheme/tinyscheme/test_transients.c
@@ -3,6 +3,56 @@
 #include "scheme.c"
 #include <assert.h>
 
+/* An allocator that allows us to check on things */
+
+typedef struct test_allocation_tag {
+	struct test_allocation_tag *next;
+	int freed;
+	char memory[1];
+} test_allocation_t;
+
+test_allocation_t *allocations = 0;
+
+void *test_malloc(size_t size)
+{
+	test_allocation_t *a = (test_allocation_t *)malloc(sizeof (test_allocation_t) + size);
+	a->freed = 0;
+	a->next = allocations;
+	allocations = a;
+	return &a->memory;
+}
+
+void test_free(void *ptr)
+{
+	test_allocation_t *a;
+
+	for (a = allocations; a; a = a->next) {
+		if ((void *)&a->memory == ptr) {
+			assert(!a->freed);
+			a->freed = 1;
+			return;
+		}
+	}
+
+	assert(0); // Was not allocated with test_malloc()
+}
+
+void reset_test_allocator()
+{
+	test_allocation_t *a, *next;
+
+	a = allocations;
+	while (a) {
+		next = a->next;
+		free(a);
+		a = next;
+	}
+
+	allocations = 0;
+}
+
+/* Tests */
+
 void check_transients_is_initialized_to_null(scheme *sc)
 {
 	assert(!sc->transients);
@@ -48,11 +98,12 @@ void run(void (* check) (scheme *))
 {
 	scheme sc;
 	memset(&sc, 0xfd, sizeof(sc));
-	scheme_init(&sc);
+	scheme_init_custom_alloc(&sc, test_malloc, test_free);
 
 	check(&sc);
 
 	scheme_deinit(&sc);
+	reset_test_allocator();
 }
 
 int main(int argc, char **argv)
