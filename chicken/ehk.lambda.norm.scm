0df3d85928b9fbe2f053ea0fe46ed131c8858211
Jason Felice <jason.m.felice@gmail.com>
2014-09-27 22:27:16 -0400

    binding? -> binding, returns bound symbol

--- chicken/ehk.lambda.norm.scm
+++ chicken/ehk.lambda.norm.scm
@@ -1,13 +1,16 @@
 (require-extension matchable)
 
-(module ehk.lambda.norm (binding?)
+(module ehk.lambda.norm (binding)
 
   (import scheme matchable)
 
-  (define (binding? term)
+  (define (binding term)
     (and (symbol? term)
-	 (let ((s (symbol->string term)))
-	   (and (char=? #\\ (string-ref s 0))
-		(char=? #\. (string-ref s (- (string-length s) 1)))))))
+	 (let* ((spelling (symbol->string term))
+		(len (string-length spelling)))
+	   (and (>= len 3)
+	        (char=? #\\ (string-ref spelling 0))
+		(char=? #\. (string-ref spelling (- len 1)))
+		(string->symbol (substring spelling 1 (- len 1)))))))
   
   )
--- chicken/tests/ehk.lambda.norm.scm
+++ chicken/tests/ehk.lambda.norm.scm
@@ -1,7 +1,8 @@
 
 (test-group "can distinguish binding terms"
-  (test #t (binding? '\\x.))
-  (test #t (binding? '\\y.))
-  (test #t (binding? '\\foo.))
-  (test #f (binding? 'x))
-  (test #f (binding? 42)))
+  (test 'x (binding '\\x.))
+  (test 'y (binding '\\y.))
+  (test 'foo (binding '\\foo.))
+  (test #f (binding 'x))
+  (test #f (binding 42)))
+
