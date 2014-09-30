d5662f8923d6f325451adc9d483a6281cfcfd43a
Jason Felice <jason.m.felice@gmail.com>
2014-09-29 22:26:44 -0400

    E<M/x> recurses into lambda which don't bind x

--- chicken/ehk.lambda.reduce.scm
+++ chicken/ehk.lambda.reduce.scm
@@ -6,6 +6,9 @@
   (define (E<M/x> E M x)
     (match E
       [('/ A B) `(/ ,(E<M/x> A M x) ,(E<M/x> B M x))]
+      [('\\ y F) (if (eq? x y)
+		   E
+		   `(\\ ,y ,(E<M/x> F M x)))]
       [y (if (eq? x y) M y)]))
 
   )
--- chicken/tests/ehk.lambda.reduce.scm
+++ chicken/tests/ehk.lambda.reduce.scm
@@ -1,7 +1,9 @@
 
-(test-group "E[M/x] operation"
+(test-group "E[M/x]"
   (test 42 (E<M/x> 42 79 'x))
   (test '42 (E<M/x> 'x 42 'x))
   (test '(/ 42 y) (E<M/x> '(/ x y) 42 'x))
   (test '(/ (/ y 42) 42) (E<M/x> '(/ (/ y x) x) 42 'x))
+  (test '(\\ z (/ f f)) (E<M/x> '(\\ z (/ x x)) 'f 'x))
+  (test '(\\ x (/ x x)) (E<M/x> '(\\ x (/ x x)) 'f 'x))
   )
