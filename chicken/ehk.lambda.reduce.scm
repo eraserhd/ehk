7405adf353fd8d8cdf1be82a983c8e8c5b2605a8
Jason Felice <jason.m.felice@gmail.com>
2014-09-30 15:36:39 -0400

    E<M/x> renaming to prevent name-collision case

--- chicken/ehk.lambda.reduce.scm
+++ chicken/ehk.lambda.reduce.scm
@@ -1,14 +1,23 @@
-(module ehk.lambda.reduce (E<M/x>)
+(module ehk.lambda.reduce (E<M/x> free?)
 
   (import chicken scheme)
   (use matchable)
 
+  (define (free? E x)
+    (match E
+      [('\\ y F) (if (eq? x y) #f (free? F x))]
+      [('/ A B) (or (free? A x) (free? B x))]
+      [y (eq? x y)]))
+
   (define (E<M/x> E M x)
     (match E
       [('/ A B) `(/ ,(E<M/x> A M x) ,(E<M/x> B M x))]
-      [('\\ y F) (if (eq? x y)
-		   E
-		   `(\\ ,y ,(E<M/x> F M x)))]
+      [('\\ y F) (cond
+		   [(eq? x y) E]
+		   [(and (free? M y) (free? F x))
+		    (let ((z (gensym)))
+		      `(\\ ,z ,(E<M/x> (E<M/x> F z y) M x)))]
+		   [else `(\\ ,y ,(E<M/x> F M x))])]
       [y (if (eq? x y) M y)]))
 
   )
--- chicken/tests/ehk.lambda.reduce.scm
+++ chicken/tests/ehk.lambda.reduce.scm
@@ -1,3 +1,4 @@
+(use matchable)
 
 (test-group "E[M/x]"
   (test 42 (E<M/x> 42 79 'x))
@@ -6,4 +7,6 @@
   (test '(/ (/ y 42) 42) (E<M/x> '(/ (/ y x) x) 42 'x))
   (test '(\\ z (/ f f)) (E<M/x> '(\\ z (/ x x)) 'f 'x))
   (test '(\\ x (/ x x)) (E<M/x> '(\\ x (/ x x)) 'f 'x))
-  )
+  (test '(\\ z (/ (/ + 1) (/ + 1))) (E<M/x> '(\\ z (/ x x)) '(/ + 1) 'x))
+  (test-assert (match (E<M/x> '(\\ y (/ x y)) '(/ 1 (/ y y)) 'x)
+		 [`(\\ ,(? symbol? q) (/ (/ 1 (/ y y)) ,q)) #t])))
