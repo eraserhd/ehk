78660a68062ccb642f89b23ede35b066e4d14504
Jason Felice <jason.m.felice@gmail.com>
2014-09-29 22:21:54 -0400

    E<M/x> recurses into applications

--- chicken/ehk.lambda.reduce.scm
+++ chicken/ehk.lambda.reduce.scm
@@ -5,6 +5,7 @@
 
   (define (E<M/x> E M x)
     (match E
+      [('/ A B) `(/ ,(E<M/x> A M x) ,(E<M/x> B M x))]
       [y (if (eq? x y) M y)]))
 
   )
--- chicken/tests/ehk.lambda.reduce.scm
+++ chicken/tests/ehk.lambda.reduce.scm
@@ -2,5 +2,6 @@
 (test-group "E[M/x] operation"
   (test 42 (E<M/x> 42 79 'x))
   (test '42 (E<M/x> 'x 42 'x))
-  
+  (test '(/ 42 y) (E<M/x> '(/ x y) 42 'x))
+  (test '(/ (/ y 42) 42) (E<M/x> '(/ (/ y x) x) 42 'x))
   )
