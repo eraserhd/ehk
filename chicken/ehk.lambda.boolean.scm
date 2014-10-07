aa616289a1c3b599fdf657f8f5ef402b5eab2f1f
Jason Felice <jason.m.felice@gmail.com>
2014-10-06 20:12:24 -0500

    FALSE returns its second argument

--- chicken/ehk.lambda.boolean.scm
+++ chicken/ehk.lambda.boolean.scm
@@ -1,8 +1,9 @@
-(module ehk.lambda.boolean (TRUE)
+(module ehk.lambda.boolean (TRUE FALSE)
 
   (import chicken scheme)
   (use ehk.lambda.parens)
 
   (define TRUE (parenthesize '(λt. λf. t)))
+  (define FALSE (parenthesize '(λt. λf. f)))
 
   )
--- chicken/tests/ehk.lambda.boolean.scm
+++ chicken/tests/ehk.lambda.boolean.scm
@@ -1,3 +1,5 @@
 
 (test "TRUE returns its first argument"
   42 (reduce normal-order `($ ($ ,TRUE 42) 79)))
+(test "FALSE returns its second argument"
+  79 (reduce normal-order `($ ($ ,FALSE 42) 79)))
