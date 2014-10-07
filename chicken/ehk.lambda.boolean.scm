5b0aed734bbe3ff153820fb0c0afe814d5eec538
Jason Felice <jason.m.felice@gmail.com>
2014-10-06 20:16:14 -0500

    IF TRUE returns its second argument

--- chicken/ehk.lambda.boolean.scm
+++ chicken/ehk.lambda.boolean.scm
@@ -1,9 +1,10 @@
-(module ehk.lambda.boolean (TRUE FALSE)
+(module ehk.lambda.boolean (TRUE FALSE IF)
 
   (import chicken scheme)
   (use ehk.lambda.parens)
 
   (define TRUE (parenthesize '(λt. λf. t)))
   (define FALSE (parenthesize '(λt. λf. f)))
+  (define IF (parenthesize '(λc. λt. λf. c t f)))
 
   )
--- chicken/tests/ehk.lambda.boolean.scm
+++ chicken/tests/ehk.lambda.boolean.scm
@@ -1,5 +1,8 @@
 
-(test "TRUE returns its first argument"
-  42 (reduce normal-order `($ ($ ,TRUE 42) 79)))
-(test "FALSE returns its second argument"
-  79 (reduce normal-order `($ ($ ,FALSE 42) 79)))
+(test-group "boolean logic"
+  (test "TRUE returns its first argument"
+    42 (reduce normal-order `($ ($ ,TRUE 42) 79)))
+  (test "FALSE returns its second argument"
+    79 (reduce normal-order `($ ($ ,FALSE 42) 79)))
+  (test "IF with TRUE returns its second argument"
+    42 (reduce normal-order `($ ($ ($ ,IF ,TRUE) 42) 79))))
