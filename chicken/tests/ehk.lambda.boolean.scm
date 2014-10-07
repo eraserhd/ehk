ebb4c00fba8110e39b84ba0bcbc9d8239abc0565
Jason Felice <jason.m.felice@gmail.com>
2014-10-06 20:17:02 -0500

    IF FALSE returns its third argument

--- chicken/tests/ehk.lambda.boolean.scm
+++ chicken/tests/ehk.lambda.boolean.scm
@@ -5,4 +5,6 @@
   (test "FALSE returns its second argument"
     79 (reduce normal-order `($ ($ ,FALSE 42) 79)))
   (test "IF with TRUE returns its second argument"
-    42 (reduce normal-order `($ ($ ($ ,IF ,TRUE) 42) 79))))
+    42 (reduce normal-order `($ ($ ($ ,IF ,TRUE) 42) 79)))
+  (test "IF with FALSE returns its third argument"
+    79 (reduce normal-order `($ ($ ($ ,IF ,FALSE) 42) 79))))
