1e0b89102821292912220d97b777c258ac3e1514
Jason Felice <jason.m.felice@gmail.com>
2014-12-13 11:59:50 -0500

    Some matching

--- idris/diamond/Diamond.v
+++ idris/diamond/Diamond.v
@@ -45,16 +45,28 @@ Definition is_ascii_letter (c : ascii) : Prop :=
 Definition letter_of_ascii (c : ascii) : letter + {~ is_ascii_letter c}.
   refine (let n := nat_of_ascii c in
           match ge_dec n (nat_of_ascii "A") with
-          | left ge_proof => match letter_of_nat (n - 65) 26 with
-                             | inleft l => inleft l
-                             | inright ge_proof2 => inright _
-                             end
-          | right nge_proof => inright _ 
-          end).
-  unfold not.
-  unfold is_ascii_letter.
+          | left ge_A_proof => match letter_of_nat (n - 65) 26 with
+                               | inleft l => inleft l
+                               | inright gt_Z_proof => inright _
+                               end
+          | right not_ge_A_proof => inright _ 
+          end);
+    unfold is_ascii_letter;
+    repeat match goal with
+    | [ |- ~ _ ] => unfold not; intros
+    | [ H : context[nat_of_ascii c] |- _ ] => replace (nat_of_ascii c) with n in H by crush
+    | [ H : context[nat_of_ascii "A"] |- _ ] => replace (nat_of_ascii "A") with 65 in H by crush
+    | [ H : context[nat_of_ascii "Z"] |- _ ] => replace (nat_of_ascii "Z") with 90 in H by crush
+    end.
+    crush.
+  apply plus_le_compat_l with (p := 65) in gt_Z_proof.
+  rewrite le_plus_minus_r in gt_Z_proof by assumption.
+  simpl in gt_Z_proof.
   replace (nat_of_ascii c) with n. 
   replace (nat_of_ascii "A") with 65 by crush.
+  replace (nat_of_ascii "A") with 65 in ge_A_proof by crush.
   replace (nat_of_ascii "Z") with 90 by crush.
-  unfold ge in ge_proof2.
-  rewrite plus_le_reg_l with (n := 26) (m := n - 65) (p := 65)  in ge_proof2.
+  crush.
+  crush.
+  crush.
+Defined.
