b398ddb23ebf28f0d8701202c9bb67f719c5e1f4
Jason Felice <jason.m.felice@gmail.com>
2014-12-13 23:40:24 -0500

    A diamond!

--- idris/diamond/Diamond.v
+++ idris/diamond/Diamond.v
@@ -2,6 +2,7 @@ Require Import CpdtTactics Ascii String List Arith.
 
 (* Needed for quoted character literals *)
 Local Open Scope char_scope.
+Local Open Scope string_scope.
 
 (* This is basically stolen from Fin. *)
 Inductive letterT : nat -> Set :=
@@ -16,7 +17,7 @@ Fixpoint nat_of_letter {x} (l : letterT x) : nat :=
   | LS _ l' => S (nat_of_letter l')
   end.
 
-Definition ascii_of_letter (l : letter) : ascii :=
+Definition ascii_of_letter {n} (l : letterT n) : ascii :=
   ascii_of_nat (nat_of_letter l + 65).
 
 Lemma all_nat_ge_0 : forall n : nat, n >= 0.
@@ -57,16 +58,36 @@ Definition letter_of_ascii (c : ascii) : letter + {~ is_ascii_letter c}.
     | [ H : context[nat_of_ascii c] |- _ ] => replace (nat_of_ascii c) with n in H by crush
     | [ H : context[nat_of_ascii "A"] |- _ ] => replace (nat_of_ascii "A") with 65 in H by crush
     | [ H : context[nat_of_ascii "Z"] |- _ ] => replace (nat_of_ascii "Z") with 90 in H by crush
-    end.
-    crush.
-  apply plus_le_compat_l with (p := 65) in gt_Z_proof.
-  rewrite le_plus_minus_r in gt_Z_proof by assumption.
-  simpl in gt_Z_proof.
-  replace (nat_of_ascii c) with n. 
-  replace (nat_of_ascii "A") with 65 by crush.
-  replace (nat_of_ascii "A") with 65 in ge_A_proof by crush.
-  replace (nat_of_ascii "Z") with 90 by crush.
-  crush.
-  crush.
-  crush.
+    | [ gt_Z_proof : _ - 65 >= 26  |- _ ] => apply plus_le_compat_l with (p := 65) in gt_Z_proof;
+                                             rewrite le_plus_minus_r in gt_Z_proof by assumption;
+                                             simpl in gt_Z_proof
+    end; crush.
 Defined.
+
+Fixpoint spaces (n : nat) : string :=
+  match n with
+  | O => ""
+  | S n' => (" " ++ spaces n')%string
+  end.
+
+Definition diamond_row {n} (l : letterT n) (outer_padding inner_padding : nat) : string :=
+  let l_string := String (ascii_of_letter l) "" in
+  let inner := match l with
+               | LO _ => l_string
+               | LS _ _ => l_string ++ spaces inner_padding ++ l_string
+               end in
+  spaces outer_padding ++ inner ++ spaces outer_padding.
+
+Fixpoint half_diamond {n} (l : letterT n) (outer_padding inner_padding : nat) : list string :=
+  diamond_row l outer_padding ::
+  match l with
+    | LO _ => nil
+    | LS _ l' => half_diamond l' (S outer_padding) (pred inner_padding)
+  end.
+
+Fixpoint diamond (l : letter) : list string :=
+  let inner_padding := 2 * nat_of_letter l in
+  let half := half_diamond l O inner_padding in
+  (rev half ++ tail half)%list.
+
+Eval compute in diamond (LS (LS (LS (LS LO)))).
