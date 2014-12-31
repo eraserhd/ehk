1ab618e2116dcb592f5917c17b637aff30424126
Jason Felice <jason.m.felice@gmail.com>
2014-12-30 21:27:55 -0500

    I WIN!?

--- punch22/punch22.cpp
+++ punch22/punch22.cpp
@@ -1,3 +1,6 @@
+#include <algorithm>
+#include <cassert>
+#include <cstdlib>
 #include <cstring>
 #include <functional>
 #include <iomanip>
@@ -7,6 +10,8 @@
 #include <utility>
 #include <vector>
 using namespace std;
+#define ALL(c) (c).begin(),(c).end()
+#define TR(c,i) for (typeof((c).begin()) i = (c).begin(); i != (c).end(); ++i)
 
 struct S {
     signed char first_punch[26];
@@ -26,6 +31,11 @@ struct S {
         if (a < 0) return true;
         if (a > 0) return false;
         return memcmp(mapping, rhs.mapping, sizeof(mapping)) < 0;
+    }
+    
+    bool operator == (S const& rhs) const {
+        return memcmp(first_punch, rhs.first_punch, sizeof(first_punch)) == 0 &&
+               memcmp(mapping, rhs.mapping, sizeof(mapping)) == 0;
     }
 
     int distance() const {
@@ -77,7 +87,6 @@ ostream& operator << (ostream& out, S const& rhs) {
 }
 
 int main() {
-    std::priority_queue<std::pair<int, S>, std::vector<std::pair<int, S> >, std::greater<std::pair<int, S> > > q;
     S start;
 
     int v_count = 0;
@@ -87,21 +96,16 @@ int main() {
         start.mapping[v] = v_count;
         ++v_count;
     }
-     
+
+    std::priority_queue<std::pair<int, S>, std::vector<std::pair<int, S> >, std::greater<std::pair<int, S> > > q;
     q.push(make_pair(start.distance(), start));
 
-    long ticks = 0;
+    long ticks = 10000;
     while (!q.empty()) {
         pair<int, S> top = q.top(); q.pop();
-
         if (!ticks--) {
-            ticks = 10000;
-            cout << top.second << endl;
-        }
-
-        if (top.first == 0) {
-            cout << "FINISHED:" << endl;
-            cout << top.second << endl;
+            start = top.second;
+            break;
         }
 
         for (int i = 0; i < 128; ++i)
@@ -114,6 +118,83 @@ int main() {
 
                     q.push(make_pair(next.distance(), next));
                 }
+    } 
+
+    cout << "start:" << endl;
+    cout << start << endl;
+
+    const int N = 10;
+    std::vector<pair<int, S> > gen;
+    gen.push_back(make_pair(start.distance(), start));
+    S best = start;
+    long ga_ticks = 0;
+    for (;;) {
+
+        if (!ga_ticks--) {
+            ga_ticks = 100;
+            cout << "best distance = " << best.distance() << endl;
+            cout << best << endl;
+        }
+
+        std::vector<pair<int, S> > next_gen(gen);
+        TR(gen, gen_it) {
+            S const& current = gen_it->second;
+
+            std::vector<std::pair<int, int> > unfilled;
+            for (int i = 0; i < 26; ++i)
+                for (int j = 0; j < 26; ++j) {
+
+                    bool filled = false;
+                    for (int v = 0; !filled && v < 128; ++v)
+                        if ((current.first_punch[i] & v) == current.first_punch[i] && current.mapping[v] == j)
+                            filled = true;
+
+                    if (!filled)
+                        unfilled.push_back(make_pair(i,j));
+                }
+
+            if (unfilled.empty()) {
+                cout << "I WIN!" << endl;
+                cout << current << endl;
+                return 0;
+            }
+
+            pair<int, int> which = unfilled[rand()%unfilled.size()];
+            int i = which.first;
+            int j = which.second;
+
+            // First, try for unmapped vs
+            int unmapped_v = -1;
+            int mapped_vs_length = 0;
+            int mapped_vs[128];
+            for (int v = 0; v < 128; ++v) {
+                if ((current.first_punch[i] & v) != current.first_punch[i]) continue;
+                if (current.mapping[v] == -1)
+                    unmapped_v = v;
+                else
+                    mapped_vs[mapped_vs_length++] = v;
+            }
+
+            assert(mapped_vs_length);
+            if (unmapped_v != -1) {
+                S next(current);
+                next.mapping[unmapped_v] = j;
+                next_gen.push_back(make_pair(next.distance(), next));
+            } else {
+                S next(current);
+                int v = mapped_vs[rand()%mapped_vs_length];
+                next.mapping[v] = j;
+                next_gen.push_back(make_pair(next.distance(), next));
+            }
+        }
+
+        sort(ALL(next_gen));
+        next_gen.erase(unique(ALL(next_gen)), next_gen.end());
+        if (next_gen.size() > N)
+            next_gen.erase(next_gen.begin()+N, next_gen.end());
+
+        gen = next_gen;
+        best = gen.front().second;
     }
 
     return 0;
