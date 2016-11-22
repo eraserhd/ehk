5d39601ab88865c9de1f49c51cf742060926f2f8
Jason Felice <jason.m.felice@gmail.com>
2016-11-21 19:53:35 -0500

    A plausible coproduct for JS/typeof

---
+++ category/js-typeof.js
@@ -0,0 +1,38 @@
+/*
+ * A category where Obj(JS) = JavaScript objects; Hom_JS(X,Y) is
+ * typeof(X) === Y.
+ */
+
+function coproduct(a, b) {
+  if (a === b) return a;
+  if (typeof a === b) return b;
+  if (typeof b === a) return a;
+  if (typeof a === typeof b) return typeof a;
+  return 'string';
+}
+
+function reachable(a) {
+  var result = [];
+  while (true) {
+    result.push(a);
+    if (a === 'string') break;
+    a = typeof a;
+  }
+  return result;
+}
+
+VALUES = [42, {}, [3], 'string', 'x', 'number', 'object'];
+
+function checkCoproductReachableFromAAndB() {
+  VALUES.forEach(function (a) {
+    VALUES.forEach(function (b) {
+      var cop = coproduct(a, b);
+      var as = reachable(a);
+      var bs = reachable(b);
+      console.assert(as.includes(cop));
+      console.assert(bs.includes(cop));
+    });
+  })
+}
+
+checkCoproductReachableFromAAndB();
