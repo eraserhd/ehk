686edab6b9cfd8dab23c4394b3d8403a2b1e0b5b
Jason Felice <jason.m.felice@gmail.com>
2017-01-08 13:14:11 -0500

    Seems to work?

--- spoj/PALIN/PALIN.hs
+++ spoj/PALIN/PALIN.hs
@@ -1,5 +1,5 @@
 import Control.Exception.Base (assert)
-
+ 
 half n = div (length n) 2
 
 extra n = rem (length n) 2
@@ -12,8 +12,8 @@ nearest n = leftDigits n ++ rightDigits n
 
 larger palindrome = let oldLeft = leftDigits palindrome
                         left = show ((read oldLeft :: Integer) + 1)
-                        right = reverse (take (half palindrome) left)
-                    in left ++ right
+                        right = take (half palindrome) $ repeat '0'
+                    in nearest (left ++ right)
 
 nextPalindrome input = if (read palindrome :: Integer) > (read input :: Integer)
                        then palindrome
@@ -30,6 +30,11 @@ test = assert (nextPalindrome "8" == "9") $
        assert (nextPalindrome "999" == "1001") $
        assert (nextPalindrome "123300" == "123321") $
        assert (nextPalindrome "10" == "11") $
+       assert (nextPalindrome "9" == "11") $
+       assert (nextPalindrome "555" (nextPalindrome millionOnes == onetwotwooneyadayada ) $
        "Tests passed"
+       where
+         millionOnes = take 1000000 (repeat '1')
+         onetwotwooneyadayada = take 499999 (repeat '1') ++ "22" ++ take 499999 (repeat '1')
 
 main = interact (unlines . map nextPalindrome . tail . lines)
