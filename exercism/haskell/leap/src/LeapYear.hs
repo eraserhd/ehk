ebee1bb9071d4c8966e7c2784c4fcf1711d8df2c
Jason Felice <jason.m.felice@gmail.com>
2017-08-31 14:35:35 -0400

    Clean up LeapYear.hs

--- exercism/haskell/leap/src/LeapYear.hs
+++ exercism/haskell/leap/src/LeapYear.hs
@@ -1,19 +1,21 @@
-{-# LANGUAGE Arrows #-}
-
 module LeapYear (isLeapYear) where
 
--- I'm using this problem to figure out Control.Arrow.  I make no claims that
--- arrows are a nice solution to this problem (they clearly aren't)...
 import Control.Arrow
 
 isLeapYear :: Integer -> Bool
-isLeapYear = odd . length . filter (== 0) . flip (zipWith mod) [4, 100, 400] . repeat
+isLeapYear y = divides 4 && (divides 400 || not (divides 100))
+  where
+    divides :: Integer -> Bool
+    divides x = y `mod` x == 0
 
-{-
-divides :: (Arrow a, Integral n) => n -> a n Bool
-divides x = arr (`mod` x) >>> arr (== 0)
+-- Point-free solution (not a nice solution, just for practice)
+isLeapYear' :: Integer -> Bool
+isLeapYear' = odd . length . filter (== 0) . flip (zipWith mod) [4, 100, 400] . repeat
 
-isLeapYear :: Integer -> Bool
-isLeapYear = ((((divides 100 >>> not) &&& divides 400) >>> uncurry (||)) &&&
-              divides 4) >>> uncurry (&&)
--}
+-- Arrows solution (terrible solution, just for practice)
+isLeapYear'' :: Integer -> Bool
+isLeapYear'' = ((((divides 100 >>> not) &&& divides 400) >>> uncurry (||)) &&&
+                divides 4) >>> uncurry (&&)
+               where
+                 divides :: (Arrow a, Integral n) => n -> a n Bool
+                 divides x = arr (`mod` x) >>> arr (== 0)
