5cfa259afa61bae03f31826cc178c3f46c8c45d1
Jason Felice <jason.m.felice@gmail.com>
2017-08-31 14:41:03 -0400

    Update LeapHear.hs

--- exercism/haskell/leap/src/LeapYear.hs
+++ exercism/haskell/leap/src/LeapYear.hs
@@ -3,10 +3,14 @@ module LeapYear (isLeapYear) where
 import Control.Arrow
 
 isLeapYear :: Integer -> Bool
-isLeapYear y = divides 4 && (divides 400 || not (divides 100))
-  where
-    divides :: Integer -> Bool
-    divides x = y `mod` x == 0
+isLeapYear year
+  | year `isDivisibleBy` 400 = True
+  | year `isDivisibleBy` 100 = False
+  | year `isDivisibleBy` 4   = True
+  | otherwise                = False
+
+isDivisibleBy :: Integer -> Integer -> Bool
+x `isDivisibleBy` y = x `mod` y == 0
 
 -- Point-free solution (not a nice solution, just for practice)
 isLeapYear' :: Integer -> Bool
