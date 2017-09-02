c685d90f7411b645eb387f3fe993a10bd72be1d9
Jason Felice <jason.m.felice@gmail.com>
2017-09-02 16:33:49 -0400

    Finish Squares

---
+++ exercism/haskell/difference-of-squares/src/Squares.hs
@@ -0,0 +1,10 @@
+module Squares (difference, squareOfSums, sumOfSquares) where
+
+difference :: Integral a => a -> a
+difference n = squareOfSums n - sumOfSquares n
+
+squareOfSums :: Integral a => a -> a
+squareOfSums n = (n * (n + 1) `div` 2) ^ 2
+
+sumOfSquares :: Integral a => a -> a
+sumOfSquares n = n * (n + 1) * (2*n + 1) `div` 6
