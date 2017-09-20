469cfd00bccb924b858562914cadf1093b2033d4
Jason Felice <jason.m.felice@gmail.com>
2017-09-20 09:29:23 -0400

    Don't use >>>

--- exercism/haskell/acronym/src/Acronym.hs
+++ exercism/haskell/acronym/src/Acronym.hs
@@ -1,9 +1,9 @@
 module Acronym (abbreviate) where
 
-import Control.Arrow ((&&&), (>>>))
+import Control.Arrow ((&&&))
 import Data.Char (isAlpha, isUpper, toUpper)
 
 abbreviate :: String -> String
-abbreviate = (' ' :) &&& id >>> uncurry zip >>> filter p >>> map (toUpper . snd)
+abbreviate = map (toUpper . snd) . filter p . uncurry zip . ((' ' :) &&& id)
   where
     p (c1, c2) = (not (isAlpha c1) && isAlpha c2) || (not (isUpper c1) && isUpper c2)
