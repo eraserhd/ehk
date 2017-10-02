5c68317377111fcb31883de9364f1114c2b7d3eb
Jason Felice <jason.m.felice@gmail.com>
2017-10-02 15:09:05 +0100

    Simplify

--- moreso/syntax.scm
+++ moreso/syntax.scm
@@ -1,23 +1,33 @@
 
-lambda-expr := (lambda Ty
+## L0
+
+define-expr := (define Ty
                  ((fname pat_1,1 ... pat_1,n) expr_1)
                  ...
                  ((fname pat_n,1 ... pat_n,n) expr_n))
 
-type-expr := (type (TypeCtor e_1 ... e_n Ty)
-               (DataCtor_1 Ty_1,1 ... Ty_1,n)
+type-expr := (type TypeCtor Ty
+               (DataCtor_1 Ty_1)
                ...
-               (DataCtor_2 Ty_n,1 ... Ty_n,n))
+               (DataCtor_2 Ty_n))
 
 expr := Type
      |  (forall var Ty expr)
-     |  lambda-expr
-     |  (expr_1 ... expr_n)
+     |  define-expr
+     |  (expr_1 .. expr_n)
 
 pat := var
+    |  _
     |  TypeCtor
     |  (TypeCtor e_1 ... e_n)
     |  DataCtor
     |  (DataCtor e_1 ... e_n)
 
-toplevel : (type-expr | lambda-expr)*
+toplevel : (type-expr | define-expr)*
+
+## L1
+
+type-expr := (type (TypeCtor e_1 ... e_n Ty)
+               (DataCtor_1 Ty_1,1 ... Ty_1,n)
+               ...
+               (DataCtor_2 Ty_n,1 ... Ty_n,n))
