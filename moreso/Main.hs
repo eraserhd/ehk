535c1b9046dc86d61daba5a76956bb680ddc654b
Jason Felice <jason.m.felice@gmail.com>
2017-10-03 16:28:02 +0100

    Implement parse

--- moreso/Main.hs
+++ moreso/Main.hs
@@ -1,13 +1,15 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveFunctor #-}
 
 import Control.Arrow (first)
 import Control.Applicative ((<|>))
 import Control.Monad (guard)
 import Data.Char (isSpace)
+import Data.Functor.Foldable (Fix(..), unfix, ana)
 import Data.List (intercalate)
-import GHC.Generics (Generic(..))
 
--- TODO: Escape control characters in symbols
+-- TODO:
+-- * Escape control characters in symbols
+-- * `parse` will do weird things in undefined cases
 
 data Form a = Sequence [Form a] | Atom a deriving (Eq)
 newtype Symbol = Symbol String deriving (Eq, Ord)
@@ -32,17 +34,27 @@ instance Read a => Read (Form a) where
   readsPrec _ s = first Sequence <$> readParen True readSequence s <|>
                   first Atom <$> reads s
     where
-      readSequence :: Read a => ReadS [a]
+      readSequence   :: Read a => ReadS [a]
       readSequence s =
         case reads s of
           []     -> [([], s)]
           parses -> do (x, rest) <- parses
                        first (x :) <$> readSequence rest
 
+type SExpr = Form Symbol
+
 data Expr a = Reference Symbol
             | Forall Symbol a a
             | Apply a [a]
-            deriving (Generic, Eq, Show)
+            deriving (Functor, Eq)
+
+parse'                                                                    :: SExpr -> Expr SExpr
+parse' (Atom sym@(Symbol _))                                              = Reference sym
+parse' (Sequence [Atom (Symbol "forall"), Atom var@(Symbol _), ty, expr]) = Forall var ty expr
+parse' (Sequence (x : xs))                                                = Apply x xs
+
+parse :: SExpr -> Fix Expr
+parse = ana parse'
 
 main :: IO ()
 main = putStrLn "Hello, world!"
