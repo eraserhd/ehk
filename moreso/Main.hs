4d3da7190b5cfe60b6aa3cfc8b144fe264461b27
Jason Felice <jason.m.felice@gmail.com>
2017-10-04 15:03:51 +0100

    Use ReadP

--- moreso/Main.hs
+++ moreso/Main.hs
@@ -1,17 +1,15 @@
 {-# LANGUAGE DeriveFunctor #-}
 
 import Control.Arrow (first)
-import Control.Applicative ((<|>))
-import Control.Monad (guard)
 import Data.Char (isSpace)
-import Data.Functor.Classes (Show1(..))
-import Data.Functor.Foldable (Fix(..), unfix, ana, cata)
+import Data.Functor.Foldable (Fix(..), ana, cata)
 import Data.List (intercalate)
-import Text.ParserCombinators.ReadP (readP_to_S, skipSpaces, munch1, satisfy)
+import Text.ParserCombinators.ReadP (ReadP(..), choice, readP_to_S, readS_to_P,
+                                     skipSpaces, many, munch1, char, between)
 
 -- TODO:
 -- * Escape control characters in symbols
--- * `parse` will do weird things in undefined cases
+-- * `unform` will do weird things in undefined cases
 
 data Form a = Sequence [Form a] | Atom a deriving (Eq)
 newtype Symbol = Symbol String deriving (Eq, Ord)
@@ -19,24 +17,22 @@ newtype Symbol = Symbol String deriving (Eq, Ord)
 instance Show Symbol where
   show (Symbol s) = s
 instance Read Symbol where
-  readsPrec _ s = first Symbol <$> readP_to_S (skipSpaces *> munch1 (not . notSymChar)) s
+  readsPrec _ = readP_to_S $ Symbol <$> (skipSpaces *> munch1 (not . notSymChar))
     where
       notSymChar :: Char -> Bool
       notSymChar c = c `elem` "()" || isSpace c
 
 instance Show a => Show (Form a) where
-  show (Atom a)    = show a
+  show (Atom a)      = show a
   show (Sequence xs) = "(" ++ intercalate " " (map show xs) ++ ")"
 instance Read a => Read (Form a) where
-  readsPrec _ s = first Sequence <$> readParen True readSequence s <|>
-                  first Atom <$> reads s
+  readsPrec _ = readP_to_S $ choice [ Sequence <$> between (skipSpaces *> char '(') (skipSpaces *> char ')') readSequence
+                                    , Atom <$> readS_to_P reads
+                                    ]
     where
-      readSequence   :: Read a => ReadS [a]
-      readSequence s =
-        case reads s of
-          []     -> [([], s)]
-          parses -> do (x, rest) <- parses
-                       first (x :) <$> readSequence rest
+      readSequence :: Read a => ReadP [a]
+      readSequence = many (skipSpaces *> readS_to_P reads)
+
 
 type SExpr = Form Symbol
 
@@ -44,15 +40,6 @@ data Expr a = Reference Symbol
             | Forall Symbol a a
             | Apply a [a]
             deriving (Functor, Eq)
-instance Show1 Expr where
-  liftShowsPrec sPrec _ n (Reference x)      = shows x
-  liftShowsPrec sPrec _ n (Forall x ty expr) =
-    (++ "(Forall " ++ shows x " " ++ sPrec n ty " " ++ sPrec n expr ")")
-  liftShowsPrec sPrec _ n (Apply x xs)       =
-    (++ "(" ++ sPrec n x (showSequence xs))
-    where
-      showSequence []       = ")"
-      showSequence (x : xs) = " " ++ sPrec n x (showSequence xs)
 
 unform :: SExpr -> Fix Expr
 unform = ana unform'
