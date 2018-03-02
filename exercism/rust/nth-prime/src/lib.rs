fc2a3d5f19678b47c9a5b345c2c5a83f3c9a1072
Jason Felice <jason.m.felice@gmail.com>
2018-03-02 17:25:15 -0600

    Use all() instead of any()

--- exercism/rust/nth-prime/src/lib.rs
+++ exercism/rust/nth-prime/src/lib.rs
@@ -2,7 +2,7 @@
 fn is_prime(p : u32) -> bool {
     if p == 2 { return true; }
     if p % 2 == 0 { return false; }
-    !(0..p).map(|x| 2*x+3).take_while(|x| x*x <= p).any(|x| p % x == 0)
+    (0..p).map(|x| 2*x+3).take_while(|x| x*x <= p).all(|x| p % x != 0)
 }
 
 pub fn nth(n : usize) -> Result<u32,&'static str> {
