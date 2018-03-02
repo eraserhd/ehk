092119b919e2e69cb9ff115dc60c462377fb2e6d
Jason Felice <jason.m.felice@gmail.com>
2018-03-02 15:36:10 -0600

    Rust gigasecond

---
+++ exercism/rust/gigasecond/src/lib.rs
@@ -0,0 +1,9 @@
+extern crate chrono;
+extern crate time;
+use chrono::*;
+use time::Duration;
+
+// Returns a Utc DateTime one billion seconds after start.
+pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
+    start + Duration::seconds(1_000_000_000)
+}
