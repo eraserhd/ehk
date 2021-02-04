5f9163fec4112005ffb7a8cefaa3962a111d6fd8
Jason Felice <jason.m.felice@gmail.com>
2021-02-04 10:34:47 -0500

    Simplify

--- codewars/584daf7215ac503d5a0001ae/code.cpp
+++ codewars/584daf7215ac503d5a0001ae/code.cpp
@@ -179,59 +179,39 @@ template<> struct Operator<'^'> { inline static Number evaluate(Number a, Number
 
 Value simplify(Value expr);
 
+template<char symbol>
+Value recur_and_evaluate(Value a, Value b)
+{
+    a = simplify(a);
+    b = simplify(b);
+    if (a.type == Value::Type::Number and b.type == Value::Type::Number)
+        return Operator<symbol>::evaluate(a.number(), b.number());
+    return L(symbol, a, b);
+}
+
 Value simplify(Value expr)
 {
     Value a, b;
-    if (L('+', 0, &a) == expr) return simplify(a);
-    if (L('+', &a, 0) == expr) return simplify(a);
-    if (L('+', &a, &b) == expr)
-    {
-        a = simplify(a);
-        b = simplify(b);
-        if (a.type == Value::Type::Number and b.type == Value::Type::Number)
-            return Operator<'+'>::evaluate(a.number(), b.number());
-        return L('+', a, b);
-    }
-    if (L('-', &a, 0) == expr) return simplify(a);
-    if (L('-', &a, &b) == expr)
-    {
-        a = simplify(a);
-        b = simplify(b);
-        if (a.type == Value::Type::Number and b.type == Value::Type::Number)
-            return Operator<'-'>::evaluate(a.number(), b.number());
-        return L('-', a, b);
-    }
-    if (L('*', 0, &a) == expr) return {0};
-    if (L('*', &a, 0) == expr) return {0};
-    if (L('*', 1, &a) == expr) return simplify(a);
-    if (L('*', &a, 1) == expr) return simplify(a);
-    if (L('*', &a, &b) == expr)
-    {
-        a = simplify(a);
-        b = simplify(b);
-        if (a.type == Value::Type::Number and b.type == Value::Type::Number)
-            return Operator<'*'>::evaluate(a.number(), b.number());
-        return L('*', a, b);
-    }
-    if (L('/', &a, 1) == expr) return simplify(a);
-    if (L('/', &a, &b) == expr)
-    {
-        a = simplify(a);
-        b = simplify(b);
-        if (a.type == Value::Type::Number and b.type == Value::Type::Number)
-            return Operator<'/'>::evaluate(a.number(), b.number());
-        return L('/', a, b);
-    }
-    if (L('^', &a, 1) == expr) return simplify(a);
-    if (L('^', &a, 0) == expr) return {1};
-    if (L('^', &a, &b) == expr)
-    {
-        a = simplify(a);
-        b = simplify(b);
-        if (a.type == Value::Type::Number and b.type == Value::Type::Number)
-            return Operator<'^'>::evaluate(a.number(), b.number());
-        return L('^', a, b);
-    }
+    if (L('+', 0, &a) == expr)  return simplify(a);
+    if (L('+', &a, 0) == expr)  return simplify(a);
+    if (L('+', &a, &b) == expr) return recur_and_evaluate<'+'>(a, b);
+    if (L('-', &a, 0) == expr)  return simplify(a);
+    if (L('-', &a, &b) == expr) return recur_and_evaluate<'-'>(a, b);
+    if (L('*', 0, &a) == expr)  return {0};
+    if (L('*', &a, 0) == expr)  return {0};
+    if (L('*', 1, &a) == expr)  return simplify(a);
+    if (L('*', &a, 1) == expr)  return simplify(a);
+    if (L('*', &a, &b) == expr) return recur_and_evaluate<'*'>(a, b);
+    if (L('/', &a, 1) == expr)  return simplify(a);
+    if (L('/', &a, &b) == expr) return recur_and_evaluate<'/'>(a, b);
+    if (L('^', &a, 1) == expr)  return simplify(a);
+    if (L('^', &a, 0) == expr)  return {1};
+    if (L('^', &a, &b) == expr) return recur_and_evaluate<'^'>(a, b);
+    if (L('s', &a) == expr)     return L('s', simplify(a));
+    if (L('c', &a) == expr)     return L('c', simplify(a));
+    if (L('t', &a) == expr)     return L('t', simplify(a));
+    if (L('e', &a) == expr)     return L('e', simplify(a));
+    if (L('l', &a) == expr)     return L('l', simplify(a));
     return expr;
 }
 
