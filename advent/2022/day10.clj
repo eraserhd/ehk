38fff3c64e36f3cf9e24ecf87a9eae6f43ea983b
Jason Felice <jason.m.felice@gmail.com>
2022-12-11 12:52:10 -0500

    Day 10, part 1

---
+++ advent/2022/day10.clj
@@ -0,0 +1,169 @@
+(ns day10)
+
+(def example-input "noop
+addx 3
+addx -5")
+
+(def larger-input "addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop")
+(def input (slurp "day10.txt"))
+
+(defn parse [input]
+  (read-string (str "(" input ")")))
+
+(defn solve [input]
+  (->> (parse input)
+       (map #(if (number? %) % 0))
+       (reductions + 1)
+       (drop 19)
+       (partition 1 40)
+       flatten
+       (map * (iterate (partial + 40) 20))
+       (reduce +)))
+
+(= 13140 (solve larger-input))
+(= 17940 (solve input))
