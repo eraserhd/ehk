ff3449e87718e57e93d7202025345f138b304309
Jason Felice <jason.m.felice@gmail.com>
2025-03-21 09:34:03 -0400

    Add ds env

---
+++ harvardx/data-science/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "HarvardX Data Science environment";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            R
+            rstudio
+            rPackages.dslabs
+            rPackages.tidyverse
+          ];
+        };
+      });
+}
