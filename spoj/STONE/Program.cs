8a71afd70a5c880209eb0e913ae3ea24580e32cb
Jason Felice <jason.m.felice@gmail.com>
2025-10-14 19:37:00 -0400

    Remove \r

--- spoj/STONE/Program.cs
+++ spoj/STONE/Program.cs
@@ -1,36 +1,36 @@
 ﻿using System;
+using System.Linq;
 
-namespace STONE
+struct Point
 {
-    struct Point
+    public int X, Y;
+    public Point(int x, int y) { X = x; Y = y; }
+
+    public Point Adjust(int xd, int yd) => new Point(X+xd, Y+yd);
+}
+
+class Program
+{
+    static Point Solve(Point[] points)
     {
-        public int X, Y;
-        public Point(int x, int y) { X = x; Y = y; }
+        return new Point(0, 0);
     }
 
-    internal class Program
+    static void Main(string[] args)
     {
-        static Point solve(Point[] points)
+        int T = int.Parse(Console.ReadLine());
+        for (int t = 0; t < T; t++)
         {
-            return new Point(0, 0);
-        }
-
-        static void Main(string[] args)
-        {
-            int T = int.Parse(Console.ReadLine());
-            for (int t = 0; t < T; t++)
+            int N = int.Parse(Console.ReadLine());
+            Point[] points = new Point[N];
+            for (int i = 0; i < N; i++)
             {
-                int N = int.Parse(Console.ReadLine());
-                Point[] points = new Point[N];
-                for (int i = 0; i < N; i++)
-                {
-                    int[] elts = Console.ReadLine().Split().Select(int.Parse).ToArray();
-                    points[i] = new Point(elts[0], elts[1]);
-                }
-
-                Point ans = solve(points);
-                Console.WriteLine($"{ans.X} {ans.Y}");
+                int[] elts = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                points[i] = new Point(elts[0], elts[1]);
             }
+
+            Point ans = Solve(points);
+            Console.WriteLine($"{ans.X} {ans.Y}");
         }
     }
 }
