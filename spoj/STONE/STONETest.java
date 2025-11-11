0f45f5e69cab638aa7031ee10b3af98ba5db9e37
Jason Felice <jason.m.felice@gmail.com>
2025-11-11 09:33:38 -0500

    Some TDD iterations

--- spoj/STONE/STONETest.java
+++ spoj/STONE/STONETest.java
@@ -1,38 +1,42 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.awt.geom.Point2D;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 class STONETest {
-
 	@Test
-	void Triangle_area() {
-		assertEquals(3.0*5.0/2.0, new Triangle(new Point(3,0), new Point(0,5)).area(), 1e-5);
+	void Can_compute_signed_area() {
+		var t1 = new Triangle(new Point2D.Double(0,5), new Point2D.Double(3,0));
+		assertEquals(0.5*5*3, Math.abs(t1.signedArea()));
+		var t2 = new Triangle(new Point2D.Double(3,0), new Point2D.Double(0,5));
+		assertEquals(-t1.signedArea(), t2.signedArea());
 	}
 	
 	@Test
-	void Triangle_leftAndRightArea_positive_x() {
-		var t = new Triangle(new Point(5,1), new Point(1, 5));
-		var lra = t.leftAndRightArea(Math.PI/4.0);
-		assertEquals(lra[0], lra[1], 1e-5);
+	void Can_make_triangles_from_points() {
+		var ts = Triangle.fromPoints(new Point2D.Double[] {
+				new Point2D.Double(-5,-5),
+				new Point2D.Double(-5, 5),
+				new Point2D.Double( 5, 5),
+				new Point2D.Double( 5,-5),
+		});
+		var area = Arrays.stream(ts).mapToDouble(Triangle::signedArea).sum();
+	    assertEquals(100.0, Math.abs(area));
 	}
 	
 	@Test
-	void Triangle_leftAndRightArea_negative_x() {
-		var t = new Triangle(new Point(-5,1), new Point(-1, 5));
-		var lra = t.leftAndRightArea(Math.PI*3/4.0);
-		assertEquals(lra[0], lra[1], 1e-5);
-	}
-	
-	@Test
-	void Test_case_1() {
-		var points = new Point[] {
-				new Point(5, 0),
-				new Point(0, 5),
-				new Point(-5, 0),
-				new Point(0, -5),
+	void moveOrigin_moves_points_to_honor_origin() {
+		var points = new Point2D.Double[] {
+			new Point2D.Double(10,5),
+			new Point2D.Double(4, 6),
+			new Point2D.Double(-2, -2),
 		};
-		final Point result = STONE.solve(points);
-		assertEquals(new Point(6, 0), result);
+		var origin = new Point2D.Double(5,5);
+		var adjusted = PointUtils.moveOrigin(origin, points);
+		assertEquals(new Point2D.Double(5,0), adjusted[0]);
+		assertEquals(new Point2D.Double(-1,1), adjusted[1]);
+		assertEquals(new Point2D.Double(-7,-7), adjusted[2]);
 	}
-
 }
