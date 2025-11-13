84d3819631e31aa3b879c294c31fbbd5500454b4
Jason Felice <jason.m.felice@gmail.com>
2025-11-13 12:17:26 -0500

    Find origins

--- spoj/STONE/STONE.exs
+++ spoj/STONE/STONE.exs
@@ -1,136 +1,64 @@
 defmodule Point do
-  def parse(p) do
-    String.split(p, " ") |> Enum.map(fn coord -> Integer.parse(coord) |> elem(0) end) |> List.to_tuple
+  defstruct x: 0, y: 0
+
+  def left_bottom_origin(ps), do: %Point{
+    x: Enum.min(Enum.map(ps, fn %Point{x: x} -> x end)) - 100,
+    y: Enum.min(Enum.map(ps, fn %Point{y: y} -> y end)) - 100,
+  }
+
+  def right_bottom_origin(ps), do: %Point{
+    x: Enum.max(Enum.map(ps, fn %Point{x: x} -> x end)) + 100,
+    y: Enum.min(Enum.map(ps, fn %Point{y: y} -> y end)) - 100,
+  }
+end
+
+defmodule Triangle do
+  defstruct [:a, :b]
+
+  # Per the shoelace formula
+  def signed_area(%__MODULE__{a: %Point{x: ax, y: ay}, b: %Point{x: bx, y: by}}), do: (ay + by)*(ax - bx)/2.0
+
+  def from_points(ps), do: Enum.zip(ps, tl(ps) ++ [hd(ps)]) |> Enum.map(fn {a, b} -> %Triangle{a: a, b: b} end)
+end
+
+if System.get_env("RUN_TESTS") do
+  ExUnit.start()
+
+  defmodule TriangleTest do
+    use ExUnit.Case
+    test "Triangles can compute signed area" do
+      t1 = %Triangle{a: %Point{x: 0, y: 7}, b: %Point{x: 3, y: 0}}
+      assert 7.0*3.0/2.0 == abs(Triangle.signed_area(t1))
+      t2 = %Triangle{a: %Point{x: 3, y: 0}, b: %Point{x: 0, y: 7}}
+      assert -Triangle.signed_area(t2) == Triangle.signed_area(t1)
+    end
+
+    test "Can make Triangles from Points" do
+      ts = Triangle.from_points([
+        %Point{x: -10, y: 10},
+        %Point{x: 10, y: 10},
+        %Point{x: 10, y: -10},
+        %Point{x: -10, y: -10},
+      ])
+      assert [
+        %Triangle{a: %Point{x: -10, y: 10}, b: %Point{x: 10, y: 10}},
+        %Triangle{a: %Point{x: 10, y: 10}, b: %Point{x: 10, y: -10}},
+        %Triangle{a: %Point{x: 10, y: -10}, b: %Point{x: -10, y: -10}},
+        %Triangle{a: %Point{x: -10, y: -10}, b: %Point{x: -10, y: 10}},
+      ] == ts
+      assert 20*20 == abs(ts |> Enum.map(&Triangle.signed_area/1) |> Enum.sum())
+    end
+
+    test "Can find origins from a set of points" do
+      ps = [
+        %Point{x: -10, y: 10},
+        %Point{x: 10, y: 10},
+        %Point{x: 10, y: -10},
+        %Point{x: -10, y: -10},
+      ]
+      assert %Point{x: -110, y: -110} == Point.left_bottom_origin(ps)
+      assert %Point{x: 110, y: -110} == Point.right_bottom_origin(ps)
+    end
   end
 end
 
-defmodule Points do
-  defp adjust1(v1, v2, :min), do: min(v1,v2)
-  defp adjust1(v1, v2, :max), do: max(v1,v2)
-
-  defp extra_offset(v, :min), do: v-1
-  defp extra_offset(v, :max), do: v+1
-
-  def offset([{x, y} | rest] = points, x_adjust, y_adjust) do
-    {x_offset, y_offset} = rest |>
-      Enum.reduce({x, y}, fn {x, y}, {accx, accy} ->
-        {adjust1(accx, x, x_adjust), adjust1(accy, y, y_adjust)}
-      end)
-    x_offset = extra_offset(x_offset, x_adjust)
-    y_offset = extra_offset(y_offset, y_adjust)
-
-    adjusted_points = points |>
-      Enum.map(fn {x, y} -> {x-x_offset, y-y_offset} end)
-
-    {x_offset, y_offset, adjusted_points}
-  end
-end
-
-defmodule Stone do
-  def read_integer, do: IO.gets("") |> Integer.parse |> elem(0)
-
-  def repeat(0, _), do: :ok
-  def repeat(n, fun) do
-    fun.()
-    repeat(n-1, fun)
-  end
-
-  def bisect(points, x_adjust, y_adjust) do
-    # The algorithm requires 0,0 to be outside of the polygon
-    {x_offset, y_offset, adjusted_points} = Points.offset(points, x_adjust, y_adjust)
-
-    # Using the Shoelace formula, modifying the triangle interpretation:
-    #
-    # Make a list of { start_angle, area_per_radian }, so
-    # that a shape that has [{ 0, 10 }, { 0.5, 0 }] would have an area
-    # of 5, since there's 0.5 radians between the two angles and the shape
-    # has an area of 10 per radian.
-    #
-    start_angle_to_area_per_radian = (adjusted_points ++ [hd(adjusted_points)]) |>
-      Enum.chunk_every(2, 1, :discard) |>
-      Enum.flat_map(fn [{x1, y1}, {x2, y2}] ->
-        [a1, a2] = Enum.sort([:math.atan2(y1,x1), :math.atan2(y2,x2)])
-
-        area_per_rad = (x1 * y2 - x2 * y1) / 2 / (a2-a1)
-        [
-          {a1, area_per_rad},
-          {a2, -area_per_rad}
-        ]
-      end) |>
-      Enum.group_by(&elem(&1,0)) |>
-      Enum.map(fn {k, v} ->
-        sum = v |>
-          Enum.map(& elem(&1, 1)) |>
-          Enum.reduce(0, &+/2)
-        {k, sum}
-      end) |>
-      Enum.sort |>
-      Enum.scan(fn {angle, area_per_radian}, {_prior_angle, prior_area_per_radian} ->
-        {angle, prior_area_per_radian + area_per_radian}
-      end)
-
-    # Use that to find the full area
-    total_area = start_angle_to_area_per_radian |>
-      Enum.chunk_every(2, 1, :discard) |>
-      Enum.map(fn [{a1, apr}, {a2, _}] ->
-        (a2-a1)*apr
-      end) |>
-      Enum.reduce(0, &+/2)
-
-    # Find the angle that bisects the full area
-    {half_angle, _area} = start_angle_to_area_per_radian |>
-      Enum.chunk_every(2, 1, :discard) |>
-      Enum.reduce_while({0, 0}, fn [{a1, area_per_radian1}, {a2, _area_per_radian2}], {_, subtotal} ->
-        next_subtotal = subtotal + (a2-a1)*area_per_radian1
-        if next_subtotal <= total_area/2 do
-          {:cont, {a2, next_subtotal}}
-        else
-          ## THIS IS WRONG!!
-          ##
-          ## It assumes that we can take a percent of the angular travel to get
-          ## an equivalent percent of the area of that part of the shape...
-          ## but that doesn't work because triangles aren't pie-peice shaped, so
-          ## the area isn't smoothly distributed across the angle.
-          ##
-          ## By this point, we've lost the points, so we can't pythogreate the answer,
-          ## and area_per_radian is acutally useless because the whole point of it
-          ## was to do this thing here.
-          ##
-          percent = (total_area/2 - subtotal) / ((a2 - a1)*area_per_radian1)
-          angle = a1 + (a2-a1)*percent
-          {:halt, {angle, subtotal + (angle - a1)*area_per_radian1}}
-        end
-      end)
-
-    {{x_offset, y_offset}, half_angle}
-  end
-
-  def solve(points) do
-    # Find two lines that bisect the shape
-    {{x1, y1}, a1} = bisect(points, :min, :min)
-    {{x2, y2}, a2} = bisect(points, :max, :min)
-
-    m1 = :math.tan(a1)
-    m2 = :math.tan(a2)
-
-    # Find the point on both lines
-    x = (m1 * x1 - y1 - m2 * x2 + y2) / (m1 - m2)
-    y = m1 * (x - x1) + y1
-    {x, y}
-  end
-
-  def process_case do
-    n = read_integer()
-    points = IO.stream(:stdio, :line) |> Stream.take(n) |> Stream.map(&Point.parse/1) |> Enum.to_list
-    {x, y} = solve(points)
-    IO.puts "#{:erlang.float_to_binary(x, decimals: 2)} #{:erlang.float_to_binary(y, decimals: 2)}"
-  end
-
-  def run do
-    cases = read_integer()
-    repeat(cases, &process_case/0)
-  end
-
-end
-
-Stone.run
