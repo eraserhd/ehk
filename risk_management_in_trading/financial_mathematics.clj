d20f92ac60a253c78b9166f4e9ca1007e1e67c98
Jason Felice <jason.m.felice@gmail.com>
2026-08-11 14:06:18 -0400

    Fix standardized central moments

--- risk_management_in_trading/financial_mathematics.clj
+++ risk_management_in_trading/financial_mathematics.clj
@@ -1,4 +1,33 @@
-(ns financial-mathematics)
+(ns financial-mathematics
+ (:import
+  (org.apache.commons.math3.distribution NormalDistribution)))
 
 (defn mean [series]
   (/ (reduce + series) (count series)))
+
+(defn standardized-central-moment
+  [order series]
+  (let [center (mean series)]
+    (/ (transduce (map #(Math/pow (- % center) order)) + series)
+       (-> (transduce (map #(Math/pow (- % center) 2)) + series)
+           (Math/pow (/ order 2))))))
+
+(def stddev (partial standardized-central-moment 2))
+(def skewness (partial standardized-central-moment 3))
+(def kurtosis (partial standardized-central-moment 4))
+
+(def test-series
+  (let [dist (NormalDistribution. 0.0 1.0)]
+    (into []
+          (map (fn [_]
+                 (.sample dist)))
+          (range 100000))))
+
+;; The first standardized central moment is always zero
+(standardized-central-moment 1 test-series)
+
+;; Should be close to zero
+(mean test-series)
+(stddev test-series)
+(skewness test-series)
+(kurtosis test-series)
--- risk_management_in_trading/user.clj
+++ risk_management_in_trading/user.clj
@@ -1,3 +1,6 @@
 (ns user
  (:require
+  [clojure.repl :refer [apropos doc dir source find-doc]]
+  [clojure.repl.deps :refer :all]
+  [clojure.tools.namespace.repl :refer [refresh]]
   [scicloj.clay.v2.api :as clay]))
