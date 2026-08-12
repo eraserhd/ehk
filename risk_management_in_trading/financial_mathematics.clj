e6fefc6cca02f96531c38f9a93e71870e8ff9774
Jason Felice <jason.m.felice@gmail.com>
2026-08-12 09:27:16 -0400

    Refactor

--- risk_management_in_trading/financial_mathematics.clj
+++ risk_management_in_trading/financial_mathematics.clj
@@ -1,16 +1,25 @@
 (ns financial-mathematics
+ (:require
+  [scicloj.kindly.v4.kind :as kind])
  (:import
   (org.apache.commons.math3.distribution NormalDistribution)))
+
+^:kindly/hide-code
+(kind/hiccup
+ [:h3 "Here's some comments."])
 
 (defn mean [series]
   (/ (reduce + series) (count series)))
 
 (defn standardized-central-moment
   [order series]
-  (let [center (mean series)]
-    (/ (transduce (map #(Math/pow (- % center) order)) + series)
-       (-> (transduce (map #(Math/pow (- % center) 2)) + series)
-           (Math/pow (/ order 2))))))
+  (let [center   (mean series)
+        centered (map #(- % center) series)
+        stddev   (-> (transduce (map #(Math/pow % 2)) + centered)
+                     (/ (count centered))
+                     Math/sqrt)]
+    (/ (transduce (map #(Math/pow % order)) + centered)
+       (Math/pow stddev (/ order)))))
 
 (def stddev (partial standardized-central-moment 2))
 (def skewness (partial standardized-central-moment 3))
@@ -23,11 +32,27 @@
                  (.sample dist)))
           (range 100000))))
 
-;; The first standardized central moment is always zero
-(standardized-central-moment 1 test-series)
+(defn check [actual passes?]
+  (let [color (if (passes? actual)
+                "green"
+                "red")]
+    (kind/hiccup
+     [:div {:style {:background-color color}} actual])))
 
-;; Should be close to zero
-(mean test-series)
-(stddev test-series)
+(defn close-to [goal delta]
+  (fn [actual]
+    (< (Math/abs (- goal actual)) delta)))
+
+;; The first standardized central moment is always zero
+(check (standardized-central-moment 1 test-series)
+       (close-to 0 1e-6))
+
+(check (mean test-series)
+       (close-to 0 1e-2))
+
+(check (stddev test-series)
+       (close-to 1.0 1e-6))
 (skewness test-series)
-(kurtosis test-series)
+
+(check (kurtosis test-series)
+       (close-to 3.0 0.1))
