9a96e6a64a11af4bfc574c8f1056c3c6de97213e
Jason Felice <jason.m.felice@gmail.com>
2026-08-19 11:41:55 -0400

    Start Black-Scholes model

---
+++ risk_management_in_trading/black_scholes.clj
@@ -0,0 +1,28 @@
+(ns black-scholes
+ (:require
+  [scicloj.kindly.v4.kind :as kind]
+  [scicloj.kindly.v4.api :as kindly])
+ (:import
+  (org.apache.commons.math3.distribution NormalDistribution)))
+
+^:kindly/hide-code
+(def md (comp kindly/hide-code kind/md))
+^:kindly/hide-code
+(def tex (comp kindly/hide-code kind/tex))
+
+(defn Φ [^double x]
+  (.cumulativeProbability (NormalDistribution. 0.0 1.0) x))
+
+(md "## Black-Scholes Model
+
+Black-Scholes is a model for predicting the prices of European options
+(although it can be used for American options, since it's generally is
+better to sell unexpired options than to exercise them).
+
+")
+
+(tex "C = Se^{(b-r)T}\\Phi(d_1) - Xe^{-rT}\\Phi(d_2)")
+(tex "P = Xe^{-rT}\\Phi(-d_2) - Se^{(b-r)T}\\Phi(d_1)")
+(tex "d_1 = \\frac{\\ln{\\left(\\frac S X\\right)} + (b + \\frac 1 2\\sigma^2)T}{\\sigma\\sqrt T}")
+(tex "d_2 = \\frac{\\ln{\\left(\\frac S X\\right)} + (b - \\frac 1 2\\sigma^2)T}{\\sigma\\sqrt T} = d_1 - \\sigma\\sqrt T")
+
